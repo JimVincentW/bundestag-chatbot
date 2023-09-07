30a7ea7c2035b76cbb08de36795214df705f05b9
Jim Wagner <jimvwagner@googlemail.com>
2023-09-08 00:32:29 +0200

    chaining

--- hardcoded.py
+++ hardcoded.py
@@ -1,45 +1,54 @@
 import os
-import sys
-import fitz
-import langchain
 from langchain.chat_models import ChatOpenAI
 
-type = "Vorgang"
-id = 300955
-intiative = ["gruene", "spd", "fdp")]
-important_files = ["BT-Drucksache 20/7356", "1. Beratung BT-Plenarprotokoll 20/113, S. 13947C-13959B"]
-important_item_1 = important_files[0]
-important_item_2 = important_files[1]
+class LegislativeDocumentProcessor:
 
+    def __init__(self):
+        self.api_key = os.getenv("OPENAI_API_KEY")
+        self.document = {
+            "type": "Vorgang",
+            "id": 300955,
+            "intiative": ["gruene", "spd", "fdp"],
+            "important_files": [
+                "BT-Drucksache 20/7356",
+                "1. Beratung BT-Plenarprotokoll 20/113, S. 13947C-13959B"
+            ]
+        }
+    
 
+    def ModelA(self):
+        instructions = {
+            "role": "referent",
+            "team_role": "teamleiter",
+            "action": "zusammenfassen",
+            "input": ["Titel", "Struturzeilen", "Keywords", "Text"],
+            "output": ["Keywords", "Topics", "Summary", "Important Sections", "OpenBookMemory"],
+            "special_instruction": ["CreateOpenBookMemory", "Structurise"]
+        }
 
-#Model A 
-# Purpose: Summarize, suggest Topics, extract Keywords
-# Input: Text Headline & Bold Statement
-# Output: Keywords, Topics, Summary
-# Value Add: Pointing to the important sections of the Document, Starting OpenBookMemory
+        model = ChatOpenAI(
+            temperature=0,
+            openai_api_key=self.api_key,
+            model="gpt-3.5-turbo-0613"
+        )
 
+        prompt = f"""
+        Du bist {instructions["role"]} und sollst {instructions["type"]}s Dokumente des Bundestags {instructions["action"]}.
+        Dafür hast du folgendes Dokument zur Verfügung:
+        {instructions["input"][0]}: {self.document["important_files"][0]}.
 
-instruction_overhead_chain_1 = [
-    "role" : "referent",
-    "team_role" : "teamleiter",
-    "action" : "zusammenfassen", 
-    "type" : "Vorgang",
-    "input" : ["Titel", "Struturzeilen", "Keywords", "Text"],
-    "output" : ["Keywords", "Topics", "Summary", "Important Sections", "OpenBookMemory"]
-    "special_instruction" : "CreateOpenBookMemory", "Structurise"
-]
+        Du arbeitest als Teil eines Teams.
+        Deine Rolle ist {instructions["team_role"]}.
 
-MODEL_A = ChatOpenAI(temperature=0, openai_api_key=os.getenv("OPENAI_API_KEY"), model="gpt-3.5-turbo-0613")
+        Bitte fasse auch für das dynamische OpenBookMemory zusammen.
+        """
 
-A_Template = f"""
-Du bist {instruction_overhead_chain_1["role"]} und sollst {instruction_overhead_chain_1["type"]}s Dokumente des Bundestags {instruction_overhead_chain_1["action"]}.
-Dafür hast du folgendes Dokument zur Verfügung:
-{instruction_overhead_chain_1["input"][0]}: {important_item_1}.
+        response = model.generate(prompt)
+        # Process the response as needed
+        # Update OpenBookMemory, etc.
+        return response
 
-Du arbeitst als Teil eines Teams.
-Deine Rolle ist {instruction_overhead_chain_1["team_role"]}.
+    # Models B, C, D, E can be similarly defined
 
-
-
-"""
+processor = LegislativeDocumentProcessor()
+processor.ModelA()
